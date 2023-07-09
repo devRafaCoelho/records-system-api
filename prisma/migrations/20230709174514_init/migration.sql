b253b7f1309967282e7f10c2b3cc054d49c33454
Rafael <devrafalcoelho@gmail.com>
2023-07-09 14:51:06 -0300

    created api base structure

---
+++ prisma/migrations/20230709174514_init/migration.sql
@@ -0,0 +1,51 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "fisrtName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT,
+    "phone" TEXT,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" SERIAL NOT NULL,
+    "fisrtName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "address" TEXT,
+    "complement" TEXT,
+    "zip_code" TEXT,
+    "district" TEXT,
+    "city" TEXT,
+    "uf" TEXT,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Record" (
+    "id" SERIAL NOT NULL,
+    "id_clients" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "due_date" TIMESTAMP(3) NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "paid_out" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Record_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Client_email_key" ON "Client"("email");
+
+-- AddForeignKey
+ALTER TABLE "Record" ADD CONSTRAINT "Record_id_clients_fkey" FOREIGN KEY ("id_clients") REFERENCES "Client"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.API_PORT, () => {
+  console.log(`API connected on port ${process.env.API_PORT}`);
+});
