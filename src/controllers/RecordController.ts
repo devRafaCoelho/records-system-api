b9e4bf5bf70addb9eaa3cd17c0ae67a385b6e59c
Rafael <devrafalcoelho@gmail.com>
2023-09-10 07:51:35 -0300

    created get record route

--- src/controllers/RecordController.ts
+++ src/controllers/RecordController.ts
@@ -41,3 +41,38 @@ export const registerRecord = async (req: Request, res: Response) => {
     return res.status(500).json({ message: 'Erro interno do servidor' });
   }
 };
+
+export const getRecord = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const record = await prisma.record.findUnique({
+      where: {
+        id: parseInt(id)
+      }
+    });
+
+    if (!record)
+      return res.status(400).json({ error: { type: 'id', message: 'Cobrança não encontrada.' } });
+
+    const getStatus = () => {
+      if (record.paid_out) return 'Paga';
+      if (new Date(record.due_date) < new Date()) return 'Vencida';
+      return 'Pendente';
+    };
+
+    const data = {
+      recordId: record.id,
+      id_clients: record.id_clients,
+      description: record.description,
+      due_date: formatDate(record.due_date),
+      value: formatValue(record.value),
+      paid_out: record.paid_out,
+      status: getStatus()
+    };
+
+    return res.status(200).json(data);
+  } catch {
+    return res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+};
