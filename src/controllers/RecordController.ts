fab8cc0781cf00247648baa4ec26d1bccc77cb82
Rafael <devrafalcoelho@gmail.com>
2023-09-10 09:55:42 -0300

    created list records route

--- src/controllers/RecordController.ts
+++ src/controllers/RecordController.ts
@@ -55,7 +55,7 @@ export const getRecord = async (req: Request, res: Response) => {
     if (!record)
       return res.status(400).json({ error: { type: 'id', message: 'Cobrança não encontrada.' } });
 
-    const getStatus = () => {
+    const setStatus = () => {
       if (record.paid_out) return 'Paga';
       if (new Date(record.due_date) < new Date()) return 'Vencida';
       return 'Pendente';
@@ -68,7 +68,7 @@ export const getRecord = async (req: Request, res: Response) => {
       due_date: formatDate(record.due_date),
       value: formatValue(record.value),
       paid_out: record.paid_out,
-      status: getStatus()
+      status: setStatus()
     };
 
     return res.status(200).json(data);
@@ -131,6 +131,106 @@ export const deleteRecord = async (req: Request, res: Response) => {
     });
 
     return res.status(204).send();
+  } catch {
+    return res.status(500).json({ message: 'Erro interno do servidor' });
+  }
+};
+
+export const listRecords = async (req: Request, res: Response) => {
+  const {
+    order,
+    status,
+    name,
+    date,
+    page: pageQuery = '1',
+    perPage: perPageQuery = '10'
+  } = req.query;
+  const page = Number(pageQuery);
+  const perPage = Number(perPageQuery);
+  const offset = (page - 1) * perPage;
+
+  try {
+    const allRecords = await prisma.record.findMany({
+      orderBy: {
+        id: order === 'desc' ? 'desc' : 'asc'
+      },
+      skip: offset,
+      take: perPage
+    });
+
+    const setStatus = (record: any) => {
+      if (record.paid_out) return 'Paga';
+      if (new Date(record.due_date) < new Date()) return 'Vencida';
+      return 'Pendente';
+    };
+
+    const clientIds = allRecords.map((record) => record.id_clients);
+
+    const clients = await prisma.client.findMany({
+      where: {
+        id: {
+          in: clientIds
+        }
+      }
+    });
+
+    let formattedRecords = allRecords.map((record) => {
+      const client = clients.find((client) => client.id === record.id_clients);
+
+      return {
+        recordId: record.id,
+        id_clients: record.id_clients,
+        description: record.description,
+        due_date: formatDate(record.due_date),
+        value: formatValue(record.value),
+        paid_out: record.paid_out,
+        status: setStatus(record),
+        clientName: `${client?.firstName} ${client?.lastName}`
+      };
+    });
+
+    if (status) {
+      formattedRecords = formattedRecords.filter((record) => record.status === status);
+
+      if (formattedRecords.length === 0) {
+        return res
+          .status(400)
+          .json({ error: { type: 'status', message: 'Nenhuma cobrança encontrada.' } });
+      }
+    }
+
+    if (name) {
+      if (typeof name === 'string') {
+        formattedRecords = formattedRecords.filter((record) =>
+          record.clientName.toLowerCase().includes(name.toLowerCase())
+        );
+      }
+
+      if (formattedRecords.length === 0) {
+        return res
+          .status(400)
+          .json({ error: { type: 'name', message: 'Nenhuma cobrança encontrada.' } });
+      }
+    }
+
+    if (date) {
+      formattedRecords = formattedRecords.filter((record) => record.due_date === date);
+
+      if (formattedRecords.length === 0) {
+        return res
+          .status(400)
+          .json({ error: { type: 'date', message: 'Nenhuma cobrança encontrada.' } });
+      }
+    }
+
+    const response = {
+      totalRecords: formattedRecords.length,
+      totalPages: Math.ceil(formattedRecords.length / perPage),
+      currentPage: page,
+      records: formattedRecords
+    };
+
+    return res.status(200).json(response);
   } catch {
     return res.status(500).json({ message: 'Erro interno do servidor' });
   }
